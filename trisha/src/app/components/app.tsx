3d40711468968e313768323bbb3268bd7fb734d4
Akanji Abayomi Hussein <akanjiabayomi2@gmail.com>
2024-11-22 13:45:14 +0100

    Trisha Project initialised, working on loading screen

---
+++ trisha/src/app/components/app.tsx
@@ -0,0 +1,23 @@
+/** @format */
+
+'use client';
+import React, { useEffect, useState } from 'react';
+import Loading from '@/app/components/loading';
+
+const EntryPoint = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  //useEffects
+  useEffect(() => {
+    const intervalID = setTimeout(() => {
+      setIsLoading(false);
+    }, 6000);
+
+    return () => clearTimeout(intervalID);
+  }, [isLoading]);
+
+  if (isLoading) return <Loading />;
+  return <div>Welcome</div>;
+};
+
+export default EntryPoint;
